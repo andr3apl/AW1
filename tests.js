1a7aad2f4a79971ff204fd1ec73337a8af103e62
andr3apl <andreapala32@gmail.com>
2024-04-28 18:32:38 +0200

    new file tests.js

---
+++ tests.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function square(x) {
+    let y = x * x;
+    return y;
+}
+let cube = function c(x) {
+    let y = square(x) * x;
+    return y;
+}
+let fourth = x => square(x) * square(x);
+
+let n = fourth(4);
+console.log(n);
+console.log(fourth);
